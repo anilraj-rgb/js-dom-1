2761da8ce4675d52dfc25a18eefc1528bb38d795
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-26 16:06:37 +0100

    [UPD] change the botton

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ button.addEventListener('click', () => {
         // Se spenta, accendo la lampada, cambio la classe che ne indica lo stato
         lamp.classList.remove('off');
         lamp.classList.add('on');
+         // cambio l'immagine con la lampadina accesa
+         lamp.src = 'img/yellow_lamp.png';
+         // Cambio il testo del bottone
+         button.textContent = 'Spegni';
+     } else {
