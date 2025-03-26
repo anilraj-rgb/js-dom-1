cf75b0dd78f5b9220fd2b1a9c7d69012bc49b090
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-26 16:05:54 +0100

    [UPD] control the lamp

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 const lamp = document.getElementById('lamp');
 const button = document.querySelector('button');
+
+
+
+button.addEventListener('click', () => {
+    // Controllo se la lampada e' accesa o spenta
+    if (lamp.classList.contains('off')) {
+        // Se spenta, accendo la lampada, cambio la classe che ne indica lo stato
+        lamp.classList.remove('off');
+        lamp.classList.add('on');
